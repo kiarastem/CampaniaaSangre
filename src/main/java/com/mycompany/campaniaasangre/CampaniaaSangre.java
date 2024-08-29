855cbf4b650979ba90570f3ef2a2f63b8b4ac738
Kiara Villarroel <141038480+kiarastem@users.noreply.github.com>
2024-08-29 12:35:45 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/campaniaasangre/CampaniaaSangre.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.campaniaasangre;
+
+/**
+ *
+ * @author kndre
+ */
+public class CampaniaaSangre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
